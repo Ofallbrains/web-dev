70ee5635813b69eaf6933dc908d21e91fb395c62
Ofallbrains <ofallbrains@gmail.com>
2023-11-07 18:45:18 +0200

    first commit

---
+++ Js/main.js
@@ -0,0 +1,24 @@
+let x = 5;
+console.log(x)
+x = 3;
+console.log(x)
+
+console.log('i love js');
+
+const greet = 'Hello';
+const name = 'Jack';
+console.log(greet + ' ' + name);
+
+const number1 = 3/0;
+console.log(number1); 
+const number2 = -3/0;
+console.log(number2); 
+const number3 = "abc"/3; 
+console.log(number3); 
+
+// BigInt value
+const value1 = 900719925124740998n;
+const result1 = value1 + 1n;
+console.log(result1); 
+
+
---
+++ opeators.js
@@ -0,0 +1,38 @@
+let x = 5;
+let y = 3;
+console.log('x + y = ', x + y);  
+console.log('x - y = ', x - y); 
+console.log('x * y = ', x * y);  
+console.log('x / y = ', x / y);
+console.log('x % y = ', x % y);  
+console.log('++x = ', ++x);
+console.log('x++ = ', x++); 
+console.log('x = ', x); 
+console.log('--x = ', --x);
+console.log('x-- = ', x--); 
+console.log('x = ', x);  
+console.log('x ** y =', x ** y);
+
+const a = 3, b = 2;
+console.log(a > b); 
+console.log(2 == 2); 
+console.log(2 == '2'); 
+console.log(3 != 2); 
+console.log('hello' != 'Hello'); 
+console.log(2 === 2); 
+console.log(2 === '2'); 
+console.log(2 !== '2'); 
+console.log(2 !== 2); 
+
+
+console.log(true && true);
+console.log(true && false); 
+
+console.log(true || false);
+
+console.log(!true);
+
+console.log('hello' + 'world');
+let c = 'JavaScript';
+c += ' tutorial'; 
+console.log(c);
