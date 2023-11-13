4f757c34376a70ac800cd9ab8935966bc58c59ed
Ofallbrains <ofallbrains@gmail.com>
2023-11-14 00:08:29 +0200

    JS control flow

--- Js/loops.js
+++ Js/loops.js
@@ -92,3 +92,17 @@ switch (a) {
         break;
 }
 console.log(`The value is ${a}`);
+
+
+// multiple case switch program
+let fruit = 'apple';
+switch(fruit) {
+    case 'apple':
+    case 'mango':
+    case 'pineapple':
+        console.log(`${fruit} is a fruit.`);
+        break;
+    default:
+        console.log(`${fruit} is not a fruit.`);
+        break;
+}
