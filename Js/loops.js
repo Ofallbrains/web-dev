da7777456b42ce787c4c09949de86026615c4f0a
Ofallbrains <ofallbrains@gmail.com>
2023-11-13 21:36:10 +0200

    JS loops

--- Js/loops.js
+++ Js/loops.js
@@ -42,3 +42,15 @@ for (let r = 1; r <= 5; r++) {
     }
     console.log(r);
 }
+
+
+for (let i = 1; i <= 3; i++) {
+
+    // second loop
+    for (let j = 1; j <= 3; j++) {
+        if (i == 2) {
+          break;
+        }
+        console.log(`i = ${i}, j = ${j}`);
+    }
+}
